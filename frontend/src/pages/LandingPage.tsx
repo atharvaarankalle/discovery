4a0a2ad799f1321e8693ea16a4a7b4b6df469a42
Michelle <100409601+mlie788@users.noreply.github.com>
2024-04-30 00:00:16 +1200

    feat: Created base background for the landing page
    Added image for stars in the assets folder and added credits in the frontend directory readme

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,28 @@
+import {styled} from "@mui/material";
+import starsBackground from '../assets/stars_background.jpg';
+
 const LandingPage = () => {
-  return <>Landing Page</>;
+  return (
+    <LandingPageBackground>
+      <StarsBackground>
+        Landing Page
+      </StarsBackground>
+    </LandingPageBackground>);
 };
+
+const LandingPageBackground = styled('div')({
+  background: 'linear-gradient(116.82deg, #272747 0%, #7E4DCD 100%)',
+  backgroundColor: '#272747',
+  height: '100vh',
+  width: '100%',
+});
+
+const StarsBackground = styled('div')({
+  backgroundImage: `url(${starsBackground})`,
+  mixBlendMode: "color-dodge",
+  height: '100vh',
+  width: '100%',
+  backgroundSize: "cover",
+});
 
 export default LandingPage;
