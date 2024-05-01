c32c88f0f30b67b9e6f3977d40290d2177b3f9cd
Tuan <tle428@aucklanduni.ac.nz>
2024-05-01 22:17:38 +1200

    removed cors for now

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../src/routes/routes";
 import dotenv from "dotenv";
-import cors from "cors";
 
 dotenv.config();
 
@@ -9,11 +8,6 @@ const app = express();
 const PORT = process.env.BACKEND_PORT ?? 3000;
 
 app.use("/", routes);
-
-// This doesn't do anything atm for api request but might help with other requests
-app.use(cors({
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
