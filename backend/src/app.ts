28f74f47b1000cbd50066f7307606c4ee9978aaf
arielyxw <wany468@aucklanduni.ac.nz>
2024-05-01 15:35:58 +1200

    Added MongoDB connection in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import routes from "../src/routes/routes";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.BACKEND_PORT ?? 3000;
+app.use(express.json());
+
+const DB_URL =
+  process.env.MONGODB_CONNECTION_STRING ??
+  "mongodb://localhost:27017/mydatabase"; // this is not implemented
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
 app.use("/", routes);
+
+const PORT = process.env.BACKEND_PORT ?? 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
