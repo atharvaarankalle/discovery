3b0c6e4b0a55a96b8701f47f520c0fc70e4aeef9
Michelle <100409601+mlie788@users.noreply.github.com>
2024-05-01 21:31:43 +1200

    feat: added the foreground planet and one in the background
    Added respective svgs

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -3,7 +3,9 @@ import starsBackground from "../assets/stars_background.jpg";
 import { DiscoveryLogoWithtext } from "../components/Logos.tsx";
 import { baseGlow, colors, landingBackground } from "../theme.ts";
 import { useEffect, useRef, useState } from "react";
-import foregroundPlanet from "../assets/foreground_planet.svg";
+import planet1 from "../assets/planet_1.svg";
+import foregroundBase from "../assets/foreground_base.svg";
+import foregroundLayer from "../assets/foreground_layer.svg";
 
 const homePrompts = [
   "song that helps you poop",
@@ -45,16 +47,8 @@ const StyledButton = styled(Button)({
 
 const LandingPageContent = styled("div")({
   position: "relative",
-  zIndex: 1,
+  zIndex: 2,
   height: "100vh",
-});
-
-const ForegroundPlanet = styled("img")({
-  position: "absolute",
-  minWidth: "100%",
-  left: "50%",
-  bottom: -50,
-  transform: "translate(-50%,0 )", // This centers the element
 });
 
 const LandingPage = () => {
@@ -71,13 +65,100 @@ const LandingPage = () => {
           }}
         >
           <Typography variant="h4">DISCOVER the next </Typography>
-          <AnimatedPrompt promptsList={homePrompts} />
+          <AnimatedPrompt />
         </Box>
         {/*<TEMP />*/}
-        <ForegroundPlanet src={foregroundPlanet} />
       </LandingPageContent>
       <StarsBackground />
+      <Planet1 />
+      <ForegroundPlanet1 />
     </LandingPageBackground>
+  );
+};
+
+const ForegroundPlanet1 = () => {
+  return (
+    <>
+      <Box
+        component="img"
+        src={foregroundBase}
+        sx={{
+          position: "absolute",
+          minWidth: "100%",
+          left: "50%",
+          bottom: -50,
+          transform: "translate(-50%,0 )", // This centers the element
+          zIndex: 1,
+        }}
+      />
+      <Box
+        component="img"
+        src={foregroundLayer}
+        sx={{
+          position: "absolute",
+          minWidth: "100%",
+          left: "50%",
+          bottom: -50,
+          transform: "translate(-50%,0 )", // This centers the element
+          zIndex: 1,
+          mixBlendMode: "hard-light",
+        }}
+      />
+    </>
+  );
+};
+
+const Planet1 = () => {
+  return (
+    <Box
+      sx={{
+        width: "400px",
+        height: "400px",
+        position: "absolute",
+        top: "50vh",
+        left: "-10vw",
+      }}
+    >
+      <Box
+        sx={{
+          // backgroundColor: "red",
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          mixBlendMode: "hard-light",
+          background:
+            "radial-gradient(50% 50% at 50% 50%, #E79DC3 44%, #D782CF 64%, #9C5AA6 87.5%, rgba(156, 90, 166, 0) 100%)",
+        }}
+      />
+      <Box
+        component="img"
+        src={planet1}
+        sx={{
+          position: "absolute",
+          width: "68%",
+          height: "68%",
+          top: "50%",
+          left: "50%",
+          mixBlendMode: "normal",
+          transform: "translate(-50%, -50%)",
+        }}
+      />
+      <Box
+        sx={{
+          // backgroundColor: "red",
+          width: "68%",
+          height: "68%",
+          position: "absolute",
+          mixBlendMode: "color-dodge",
+          backgroundColor: "#4D4078",
+          borderRadius: "500px",
+          filter: "blur(5px)",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      />
+    </Box>
   );
 };
 
