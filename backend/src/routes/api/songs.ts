5212bab79f3fb0e0a05125e6b3bb85349914fe63
Atharva Arankalle <atharvanz@gmail.com>
2024-05-02 17:59:12 +1200

    fix(spotify-api): removed duplicate checking due to issues with pagination

--- backend/src/routes/api/songs.ts
+++ backend/src/routes/api/songs.ts
@@ -37,6 +37,24 @@ const tokenMiddleware = async (req: Request, res: Response, next: any) => {
 };
 
 /**
+ * Given a track object from the Spotify API, this function extracts the relevant information and returns it in a new object.
+ * 
+ * @param trackData The track object from the Spotify API
+ * @returns An object containing the relevant information about the track
+ */
+const formatResponseData = (trackData: any) => {
+    return {
+        id: trackData.id,
+        title: trackData.name,
+        artist: trackData.artists.map((artist: any) => artist.name).join(", "),
+        album: trackData.album.name || "",
+        albumArt: trackData.album.images.find((image: any) => image.height === 300)?.url || "",
+        previewUrl: trackData.preview_url ? trackData.preview_url : undefined,
+        openInSpotifyUrl: !trackData.preview_url ? trackData.external_urls.spotify : undefined
+    };
+}
+
+/**
  * GET /api/songs/search
  * 
  * Searches for a certain number of songs from the Spotify API based on a search query provided in the query parameters.
@@ -47,6 +65,7 @@ const tokenMiddleware = async (req: Request, res: Response, next: any) => {
  * - page: The page number of the search results (default: 1)
  * 
  * Response:
+ * An array of objects, each containing the following information about a song:
  * - id: The Spotify ID of the song
  * - title: The title of the song
  * - artist: The artist(s) of the song
@@ -57,91 +76,60 @@ const tokenMiddleware = async (req: Request, res: Response, next: any) => {
  * 
  * Response Codes:
  * - 200 OK: Successful request
- * - 400 Bad Request: No song title was provided
+ * - 400 Bad Request: No search query or page number was provided
  * - 401 Unauthorized: The Spotify API access token is invalid
  * - 404 Not Found: No songs were found matching the search query
  */
-let currentSearchOffset: number = 0;
-let savedSongSearch: string | undefined = undefined;
-let previousPage: number = 1;
-let currentLimit: number = 6;
 
 router.get("/search", tokenMiddleware, async (req: Request, res: Response) => {
     // Get values from query parameters passed from the frontend
-    const searchQuery: string | undefined = req.query.searchQuery ? String(req.query.searchQuery) : undefined;
+    const searchQuery: string | undefined = req.query.searchQuery && String(req.query.searchQuery);
     const numSongs: number = Number(req.query.numSongs) || 6;
     const page: number | undefined = req.query.page ? Number(req.query.page) : undefined;
-    const responseData: any = [];
 
     // If no song title or page number is provided, send a 400 Bad Request response
-    if (searchQuery === undefined || page === undefined) {
+    if (!searchQuery || !page) {
         res.status(400).send("Please provide a song title and page number");
-        currentSearchOffset = 0;
         return;
     }
 
-    // Search for songs from the Spotify API until the number of songs requested is reached
-    while (responseData.length < numSongs) {
-        currentSearchOffset = Math.max((page * numSongs) - numSongs, currentSearchOffset);
+    try {
+        // Calculate the offset for the search query based on the page number
+        const searchOffset = (page - 1) * numSongs;
 
-        // If the search query has changed, the page has changed, or the number of songs requested has changed, reset the current search offset
-        if (savedSongSearch !== searchQuery || previousPage > page || currentLimit !== numSongs) {
-            currentSearchOffset = 0;
+        const response = await axios.get("https://api.spotify.com/v1/search", {
+            params: {
+                q: searchQuery,
+                type: "track",
+                limit: numSongs,
+                offset: searchOffset,
+            },
+            headers: {
+                Authorization: `Bearer ${SPOTIFY_API_ACCESS_TOKEN}`
+            }
+        });
+
+        // If no songs are found matching the search query, send a 404 Not Found response
+        if (response.data.tracks.items.length === 0) {
+            res.status(404).send("No songs found matching the search query");
+            return;
         }
-        savedSongSearch = searchQuery;
-        previousPage = page;
-        currentLimit = numSongs;
 
-        try {
-            const response = await axios.get("https://api.spotify.com/v1/search", {
-                params: {
-                    q: searchQuery,
-                    type: "track",
-                    limit: numSongs,
-                    offset: currentSearchOffset,
-                },
-                headers: {
-                    Authorization: `Bearer ${SPOTIFY_API_ACCESS_TOKEN}`
-                }
-            });
+        // Extract the relevant information from the Spotify API response and send it back to the frontend
+        const responseData = response.data.tracks.items.map((track: any) => {
+            return formatResponseData(track);
+        });
 
-            // If no songs are found matching the search query, send a 404 Not Found response
-            if (response.data.tracks.items.length === 0) {
-                res.status(404).send("No songs found matching the search query");
+        res.status(200).send(responseData);
+    } catch (error: any) {
+        switch (error.response.data.error.status) {
+            case 401:
+                res.status(401).send("Unauthorized");
                 return;
-            }
-
-            response.data.tracks.items.map((track: any) => {
-                currentSearchOffset++;
-                // Only add the song to the response data if the title and artist of the song do not match any songs already in the response data 
-                if (!responseData.some((song: any) => song.title === track.name && song.artist === track.artists.map((artist: any) => artist.name).join(", "))) {
-                    if (responseData.length === numSongs) {
-                        return;
-                    }
-
-                    responseData.push({
-                        id: track.id,
-                        title: track.name,
-                        artist: track.artists.map((artist: any) => artist.name).join(", "),
-                        album: track.album.name || "",
-                        albumArt: track.album.images.find((image: any) => image.height === 300)?.url || "",
-                        previewUrl: track.preview_url ? track.preview_url : undefined,
-                        openInSpotifyUrl: !track.preview_url ? track.external_urls.spotify : undefined
-                    });
-                }
-            });
-        } catch (error: any) {
-            switch (error.response.data.error.status) {
-                case 401:
-                    res.status(401).send("Unauthorized");
-                    return;
-                default:
-                    res.status(error.response.data.error.status).send(error.message);
-            }
+            default:
+                res.status(error.response.data.error.status).send(error.message);
         }
     }
-
-    res.status(200).send(responseData);
 });
 
 /**
@@ -153,6 +141,7 @@ router.get("/search", tokenMiddleware, async (req: Request, res: Response) => {
  * - id: The Spotify ID of the song
  * 
  * Response:
+ * An object containing the following information about the song:
  * - id: The Spotify ID of the song
  * - title: The title of the song
  * - artist: The artist(s) of the song
@@ -177,15 +166,7 @@ router.get("/:id", tokenMiddleware, async (req: Request, res: Response) => {
             }
         });
 
-        const responseData = {
-            id: response.data.id,
-            title: response.data.name,
-            artist: response.data.artists.map((artist: any) => artist.name).join(", "),
-            album: response.data.album.name || "",
-            albumArt: response.data.album.images.find((image: any) => image.height === 300)?.url || "",
-            previewUrl: response.data.preview_url ? response.data.preview_url : undefined,
-            openInSpotifyUrl: !response.data.preview_url ? response.data.external_urls.spotify : undefined
-        };
+        const responseData = formatResponseData(response.data);
 
         res.status(200).send(responseData);
     } catch (error: any) {
