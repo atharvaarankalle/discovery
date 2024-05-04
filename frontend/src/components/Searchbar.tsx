3f1d5f7fdeaae3dc5b77d3a006b4415249abe69e
Michelle <100409601+mlie788@users.noreply.github.com>
2024-05-05 01:38:56 +1200

    added styling for searchbar using MUI filled textfield component

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -1,79 +1,93 @@
 import { useState } from "react";
-import { Box, InputAdornment, Link, styled, TextField, } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import { Box, Link, styled, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import { colors } from "../theme";
 
-//Styling for searchbar
-const StyledTextField = styled(TextField)({
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': { borderColor: 'transparent' },
-      '&:hover fieldset': { borderColor: 'transparent' },
-      '&.Mui-focused fieldset': { 
-        borderColor: 'transparent',
-        borderWidth: '0 0 0.2rem 0',
-        borderBottomColor: colors.peach, 
-        transition: "border-bottom 0.2s ease",  
-    }},
-    background: `${colors.lightPeach}0F`,
-    borderRadius: "0.2rem",
-    borderColor: 'transparent',
-  });
+// Styling for searchbar
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  width: "100%",
+  "& .MuiFilledInput-underline::before": {
+    borderBottomColor: `${theme.palette.peach.main}`,
+  },
+  "& .MuiFilledInput-root": {
+    backgroundColor: `${theme.palette.peach.main}10`,
+    color: `${theme.palette.lightPeach.main}`,
+    ":hover:not(.Mui-focused)": {
+      "&:before": {
+        borderColor: `${theme.palette.peach.main}`,
+      },
+    },
+  },
+  "& .MuiFilledInput-root:after": {
+    backgroundColor: `${theme.palette.peach.main}10`,
+    color: `${theme.palette.lightPeach.main}`,
+  },
+
+  "& label.Mui-focused": {
+    color: `${theme.palette.peach.main}`,
+  },
+}));
 
 //Styling for clear search button
 const ClearSearchLink = styled(Link)({
-component: "button",
-variant: "body1",
-color: colors.peach,
-fontSize: '1rem', 
-fontWeight: 400, 
-fontFamily: 'Familjen Grotesk', 
-textTransform: 'none',
-':hover': {
-    textDecoration: 'underline',
-    cursor: 'pointer',
-}
+  component: "button",
+  variant: "body1",
+  color: colors.peach,
+  fontSize: "1rem",
+  fontWeight: 400,
+  fontFamily: "Familjen Grotesk",
+  textTransform: "none",
+  ":hover": {
+    textDecoration: "underline",
+    cursor: "pointer",
+  },
 });
 
 /**
  * Search bar component. No set width, therefore width should be set upon call.
  * The search is done as the user type and not upon confirm.
- * Clear search button gets 
+ * Clear search button gets
  * @returns Searchbar
  */
 
 export const Searchbar = () => {
+  const [inputValue, setInputValue] = useState("");
 
-    const [inputValue, setInputValue] = useState('');
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value);
-        //TO DO: Link with Spotify Search
-        };
-        const clearInput = () => {
-            setInputValue('');
-        };
-    return (
-        <Box>
-            <StyledTextField
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="Search"
-                fullWidth
-                InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                    <SearchIcon sx={{color: colors.peach, fontSize: '2.5rem'}}/>
-                    </InputAdornment>
-                ),
-                style: { color: colors.lightPeach },
-                }}
-            />
-
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <ClearSearchLink onClick={clearInput}>
-                Clear Search
-                </ClearSearchLink>
-            </Box>
-        </Box>
-    );
-}
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+    //TO DO: Link with Spotify Search
+  };
+  const clearInput = () => {
+    setInputValue("");
+  };
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "0.5rem",
+          mb: "0.5rem",
+        }}
+      >
+        <SearchIcon sx={{ color: colors.peach, fontSize: "2.5rem" }} />
+        <StyledTextField
+          variant="filled"
+          label="search"
+          color="peach"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <ClearSearchLink
+          onClick={clearInput}
+          underline="always"
+          color="secondary"
+        >
+          Clear search
+        </ClearSearchLink>
+      </Box>
+    </Box>
+  );
+};
