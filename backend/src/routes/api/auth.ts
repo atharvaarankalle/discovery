ed0adb0597bf741014f1bac66b7c8929376c742c
Amisha Singh <amishas171@gmail.com>
2024-05-05 15:38:54 +1200

    feat(api/auth/signup): completed signup endpoint, working successfully

--- backend/src/routes/api/auth.ts
+++ backend/src/routes/api/auth.ts
@@ -1,8 +1,47 @@
-// current route: /api/auth
-import express, { Router } from "express";
+import express, { Router, Request, Response } from "express";
+import { User, IUser } from "../../schemas/UserSchema";
+import bcrypt from "bcryptjs";
+import { getTodaysDate } from "../../utils/DateUtils";
 
 const router: Router = express.Router();
 
-// TODO: Add endpoints here
+/* SIGNUP ENDPOINT */
+router.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const { displayName, email, password, profilePic } = req.body;
+    // ensure all required user data is provided
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ error: "Please provide a display name, email, and password" });
+    }
+    // check if user with the email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "A user with this email already exists" });
+    }
 
+    // create new user object with hashed password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser: IUser = new User({
+      displayName: displayName || email,
+      email,
+      password: hashedPassword,
+      profilePic: profilePic || "",
+      accountCreationDate: getTodaysDate(),
+      streakCount: 0,
+      likedSongs: [],
+      suggestedSongs: [],
+    });
+    // save the user to the database
+    await newUser.save();
+
+    // respond with success message
+    res.status(201).json({ message: "Successful sign up!", user: newUser });
+  } catch (error) {
+    res.status(500).json({ error: "Signup has failed" });
+  }
+});
 export default router;
