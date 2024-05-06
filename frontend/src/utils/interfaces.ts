6e127dc0ac2646572dc1b021eb703e783f6e2cb8
Michelle <100409601+mlie788@users.noreply.github.com>
2024-05-06 19:29:13 +1200

    refactor: added id friend to SongData interface and updated docs
    as per pull request

--- frontend/src/utils/interfaces.ts
+++ frontend/src/utils/interfaces.ts
@@ -3,15 +3,18 @@
  */
 
 /**
- * SongData object prop types
- *    songTitle: title of the song
- *    artists: artist/s of the song
- *    album: album the song is from
- *    albumArtSrc: url link to the album art image url
- *    songAudioSrc: url link to the song preview audio (OPTIONAL)
- *    openInSpotifyUrl: url link to the song on spotify (REQUIRED FOR SONGS WITHOUT PREVIEW)
+ * @interface SongData
+ * * `id`: required number, uniquely identifies the song
+ * * `songTitle`: required string, the title of the song
+ * * `artists`: required string, the name/s of the artist/s of the song
+ * * `album`: required string, the names of the album the song is from
+ * * `albumArtSrc`: required string, the url link to the album art image url
+ * * `songAudioSrc`: optional string, containing the url link to the song's preview audio
+ * * `openInSpotifyUrl`: optional string, containing the url link to the song on spotify web
+ *
  */
 export interface SongData {
+  id: string;
   songTitle: string;
   artists: string;
   album: string;
