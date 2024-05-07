826a4e49c05a9839f8b41912afb7ae88284c0341
arielyxw <wany468@aucklanduni.ac.nz>
2024-05-07 22:37:38 +1200

    Added song cards and pagination

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,69 +1,58 @@
 import NavBar from "../components/NavBar";
-import { Typography, ThemeProvider, Grid, Button } from "@mui/material";
+import { Typography, ThemeProvider } from "@mui/material";
 import { theme } from "../theme";
-import SongCard from "../components/SongCard";
-import { useState } from "react";
-import PaginationButtons from "../components/PaginationButtons";
+import { SavedSongsContainer } from "../components/SongCardPaginationContainers";
+import { MusicPlayer } from "../components/MusicPlayer";
 
 const ProfilePage = () => {
   // Dummy user data
   const dummyUser = {
     name: "John Doe",
-    profilePic: "url_to_profile_pic",
+    profilePic:
+      "https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
     discoveryDate: "29th April 2024",
   };
 
   // Dummy data for songs
   const dummySongs = [
     {
+      id: "123123123",
       songTitle: "Song 1",
-      artist: "Artist 1",
+      artists: "Artist 1",
       album: "Album 1",
-      albumArtSrc: "url_to_album_1",
+      albumArtSrc:
+        "https://m.media-amazon.com/images/I/61uEKlSVzqL._AC_UF894,1000_QL80_.jpg",
     },
     {
+      id: "123456789",
       songTitle: "Song 2",
-      artist: "Artist 2",
+      artists: "Artist 2",
       album: "Album 2",
-      albumArtSrc: "url_to_album_2",
+      albumArtSrc: "https://i.redd.it/17iihei3zlkc1.jpeg",
     },
     {
+      id: "111222333",
       songTitle: "Song 3",
-      artist: "Artist 3",
+      artists: "Artist 3",
       album: "Album 3",
-      albumArtSrc: "url_to_album_3",
+      albumArtSrc: "https://i.redd.it/17iihei3zlkc1.jpeg",
     },
     {
+      id: "456456456",
       songTitle: "Song 4",
-      artist: "Artist 4",
+      artists: "Artist 4",
       album: "Album 4",
-      albumArtSrc: "url_to_album_4",
+      albumArtSrc:
+        "https://m.media-amazon.com/images/I/61uEKlSVzqL._AC_UF894,1000_QL80_.jpg",
     },
     {
+      id: "987654321",
       songTitle: "Song 5",
-      artist: "Artist 5",
+      artists: "Artist 5",
       album: "Album 5",
-      albumArtSrc: "url_to_album_5",
+      albumArtSrc: "https://i.redd.it/17iihei3zlkc1.jpeg",
     },
   ];
-
-  const [currentPage, setCurrentPage] = useState(0);
-  const songsPerPage = 4;
-  const startIndex = currentPage * songsPerPage;
-  const endIndex = Math.min(startIndex + songsPerPage, dummySongs.length);
-  const totalPages = Math.ceil(dummySongs.length / 4);
-
-  const nextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
-
-  const prevPage = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
-  };
-
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
 
   return (
     <>
@@ -84,10 +73,11 @@ const ProfilePage = () => {
             style={{
               width: "200px",
               height: "200px",
+              objectFit: "cover",
               borderRadius: "50%",
               marginRight: "60px",
             }}
-          ></img>
+          />
           <div>
             <Typography variant="h1">{dummyUser.name}</Typography>
             <Typography variant="subtitle1">
@@ -96,22 +86,14 @@ const ProfilePage = () => {
           </div>
         </div>
 
-        <Typography variant="h3" style={{ marginBottom: "20px" }}>
-          DISCOVERED SONGS
-        </Typography>
-        <Grid container spacing={2} style={{ marginBottom: "20px" }}>
-          {dummySongs.slice(startIndex, endIndex).map((song, index) => (
-            <Grid item xs={6} sm={6} md={6} lg={6} key={index}>
-              <SongCard songData={song} isLiked={true} type="large" />
-            </Grid>
-          ))}
-        </Grid>
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <Typography variant="h3" style={{ marginBottom: "20px" }}>
+            DISCOVERED SONGS
+          </Typography>
+          <SavedSongsContainer songs={dummySongs} />
 
-        <PaginationButtons
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        ></PaginationButtons>
+          <MusicPlayer />
+        </div>
       </ThemeProvider>
     </>
   );
