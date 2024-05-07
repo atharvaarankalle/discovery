f1a0603bb4d16567e99d5e3e4c63b495ef182532
Amisha Singh <amishas171@gmail.com>
2024-05-07 17:14:09 +1200

    fix: refactored component background to be simpler

--- frontend/src/components/DiscoverPageHeader.tsx
+++ frontend/src/components/DiscoverPageHeader.tsx
@@ -1,7 +1,5 @@
 import { Box, Button, Stack, styled } from "@mui/material";
-import starsBackground from "../assets/stars_background.jpg";
-import planet3 from "../assets/planet_3.svg";
-import planet4 from "../assets/planet_4.svg";
+import discoverPageHeader from "../assets/discover_page_header.png";
 
 import SongCard from "./SongCard";
 import CustomTypography from "./CustomTypography";
@@ -12,34 +10,11 @@ import { useNavigate } from "react-router-dom";
 /* Custom styles applied to MUI Box to be the main wrapper of this component */
 const StyledBox = styled(Box)(({ theme }) => ({
   backgroundColor: `${theme.palette.primary.dark}75`, // 75 at the end adds 46% opacity to colour hexcode
+  backgroundImage: `url(${discoverPageHeader})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
   borderRadius: "1.25rem",
 }));
-
-/* Custom styles applied to MUI Box to be the background image of the component */
-const StarsBackground = styled("div")({
-  backgroundImage: `url(${starsBackground})`,
-  backgroundPosition: "top",
-  mixBlendMode: "color-dodge",
-});
-
-/* Custom styles applied to MUI Box to position/overlay the planet3 image */
-const Planet3 = styled("div")({
-  backgroundImage: `url(${planet3})`,
-  backgroundRepeat: "no-repeat",
-  backgroundPositionX: "90rem",
-  backgroundPositionY: "-15rem",
-  borderRadius: "1.25rem",
-  backgroundSize: "25rem",
-});
-
-/* Custom styles applied to MUI Box to position/overlay the planet4 image */
-const Planet4 = styled("div")({
-  backgroundImage: `url(${planet4})`,
-  backgroundRepeat: "no-repeat",
-  backgroundPositionX: "3rem",
-  backgroundPositionY: "5rem",
-  borderRadius: "1.25rem",
-});
 
 /* Prop types for the DiscoverPageHeader component for clarity */
 interface DiscoverPageHeaderPropTypes {
@@ -61,48 +36,42 @@ const DiscoverPageHeader = ({
 
   return (
     <StyledBox>
-      <Planet4>
-        <Planet3>
-          <StarsBackground>
-            <Stack
-              spacing={3}
-              p="1.25rem"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <CustomTypography
-                variant="h3"
-                sx={{
-                  textTransform: songData ? "uppercase" : "none", // uppercase header text if user has submitted
-                  textAlign: "center",
-                }}
-              >
-                {/* toggling state of header text based on if user has submitted a song */}
-                {songData
-                  ? "Your Submission"
-                  : "Ready to share your song with the world?"}
-              </CustomTypography>
+      <Stack
+        spacing={3}
+        p="1.25rem"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <CustomTypography
+          variant="h3"
+          sx={{
+            textTransform: songData ? "uppercase" : "none", // uppercase header text if user has submitted
+            textAlign: "center",
+          }}
+        >
+          {/* toggling state of header text based on if user has submitted a song */}
+          {songData
+            ? "Your Submission"
+            : "Ready to share your song with the world?"}
+        </CustomTypography>
 
-              {/* conditional render of SongCard if user has submitted, else render the submit button */}
-              {songData ? (
-                <Box sx={{ width: "60%" }}>
-                  <SongCard type="small" songData={songData} />
-                </Box>
-              ) : (
-                <Button
-                  variant="contained"
-                  color="lightPeach"
-                  sx={{ textTransform: "uppercase" }}
-                  onClick={() => navigate("/user/prompt")}
-                >
-                  Submit a track
-                  <MusicNoteIcon sx={{ pl: 1 }} />
-                </Button>
-              )}
-            </Stack>
-          </StarsBackground>
-        </Planet3>
-      </Planet4>
+        {/* conditional render of SongCard if user has submitted, else render the submit button */}
+        {songData ? (
+          <Box sx={{ width: "50%" }}>
+            <SongCard type="small" songData={songData} />
+          </Box>
+        ) : (
+          <Button
+            variant="contained"
+            color="lightPeach"
+            sx={{ textTransform: "uppercase" }}
+            onClick={() => navigate("/user/prompt")}
+          >
+            Submit a track
+            <MusicNoteIcon sx={{ pl: 1 }} />
+          </Button>
+        )}
+      </Stack>
     </StyledBox>
   );
 };
