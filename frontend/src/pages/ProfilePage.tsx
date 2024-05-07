d25c5674dab1322ea04fbf8ceb201d16ec99199d
arielyxw <wany468@aucklanduni.ac.nz>
2024-05-07 19:20:55 +1200

    User details section with dummy user

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,51 @@
+import NavBar from "../components/NavBar";
+import { Typography, ThemeProvider, Grid, Button } from "@mui/material";
+import { theme } from "../theme";
+import SongCard from "../components/SongCard";
+import { useState } from "react";
+
 const ProfilePage = () => {
-  return <>Profile Page</>;
+  // Dummy user data
+  const dummyUser = {
+    name: "John Doe",
+    profilePic: "url_to_profile_pic",
+    discoveryDate: "29th April 2024",
+  };
+
+  return (
+    <>
+      <NavBar />
+      <ThemeProvider theme={theme}>
+        <Typography variant="h3" style={{ marginBottom: "20px" }}>
+          PROFILE
+        </Typography>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginBottom: "40px",
+          }}
+        >
+          <img
+            src={dummyUser.profilePic}
+            style={{
+              width: "200px",
+              height: "200px",
+              borderRadius: "50%",
+              marginRight: "60px",
+            }}
+          ></img>
+          <div>
+            <Typography variant="h1">{dummyUser.name}</Typography>
+            <Typography variant="subtitle1">
+              Discovering since {dummyUser.discoveryDate}
+            </Typography>
+          </div>
+        </div>
+
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default ProfilePage;
