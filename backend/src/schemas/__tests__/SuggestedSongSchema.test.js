16ed8ff665b421f948d3188722d149861581be92
Atharva Arankalle <atharvanz@gmail.com>
2024-05-08 19:39:07 +1200

    test(suggested-song-schema): added test to get all suggested songs

--- backend/src/schemas/__tests__/SuggestedSongSchema.test.js
+++ backend/src/schemas/__tests__/SuggestedSongSchema.test.js
@@ -48,3 +48,24 @@ afterAll(async () => {
     await mongoose.disconnect();
     await mongod.stop();
 });
+
+it("gets all suggested songs", async () => {
+    const suggestedSongsFromDb = await SuggestedSong.find();
+    expect(suggestedSongsFromDb).toBeTruthy();
+    expect(suggestedSongsFromDb.length).toBe(3);
+
+    expect(suggestedSongsFromDb[0].caption).toBe("Wow such an awesome song");
+    expect(suggestedSongsFromDb[0].prompt).toEqual(new mongoose.Types.ObjectId("000000000000000000000001"));
+    expect(suggestedSongsFromDb[0].spotifySongId).toBe("songId1");
+    expect(suggestedSongsFromDb[0].user).toEqual(new mongoose.Types.ObjectId("000000000000000000000001"));
+
+    expect(suggestedSongsFromDb[1].caption).toBe("This song is so cool");
+    expect(suggestedSongsFromDb[1].prompt).toEqual(new mongoose.Types.ObjectId("000000000000000000000001"));
+    expect(suggestedSongsFromDb[1].spotifySongId).toBe("songId2");
+    expect(suggestedSongsFromDb[1].user).toEqual(new mongoose.Types.ObjectId("000000000000000000000002"));
+
+    expect(suggestedSongsFromDb[2].caption).toBe("This song is so sad");
+    expect(suggestedSongsFromDb[2].prompt).toEqual(new mongoose.Types.ObjectId("000000000000000000000002"));
+    expect(suggestedSongsFromDb[2].spotifySongId).toBe("songId3");
+    expect(suggestedSongsFromDb[2].user).toEqual(new mongoose.Types.ObjectId("000000000000000000000003"));
+});
