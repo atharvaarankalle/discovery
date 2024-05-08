b56e1dd4c4d8955a6774eaadb164d096e2623852
Atharva Arankalle <atharvanz@gmail.com>
2024-05-08 17:18:18 +1200

    test(prompt-schema-test): set up initial test file

---
+++ backend/src/schemas/__tests__/PromptSchema.test.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import Prompt from "../PromptSchema";
+
+let mongod;
+
+const prompts = [
+    {
+        _id: new mongoose.Types.ObjectId("000000000000000000000001"),
+        prompt: "Happy Daytime Song",
+        date: new Date("2024-05-02T00:00:00.000+00:00")
+    },
+    {
+        _id: new mongoose.Types.ObjectId("000000000000000000000002"),
+        prompt: "Sad Nighttime Song",
+        date: new Date("2024-05-02T00:00:00.000+00:00")
+    },
+    {
+        _id: new mongoose.Types.ObjectId("000000000000000000000003"),
+        prompt: "Daydreaming scenario song",
+        date: new Date("2024-05-02T00:00:00.000+00:00")
+    }
+]
+
+beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+
+    const connectionString = mongod.getUri();
+    await mongoose.connect(connectionString);
+});
+
+beforeEach(async () => {
+    await mongoose.connection.db.dropDatabase();
+
+    const collection = await mongoose.connection.db.createCollection("prompts");
+    await collection.insertMany(prompts);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongod.stop();
+});
