07aa79e195db2c0f8f5ba8a55cb851e20c46e278
Atharva Arankalle <atharvanz@gmail.com>
2024-05-08 20:15:40 +1200

    test(user-schema): added test to get all users

--- backend/src/schemas/__tests__/UserSchema.test.js
+++ backend/src/schemas/__tests__/UserSchema.test.js
@@ -57,3 +57,33 @@ afterAll(async () => {
     await mongoose.disconnect();
     await mongod.stop();
 });
+
+it("gets all users", async () => {
+    const usersFromDb = await User.find();
+    expect(usersFromDb).toBeTruthy();
+    expect(usersFromDb.length).toBe(3);
+
+    expect(usersFromDb[0].email).toBe("user1@test.com");
+    expect(usersFromDb[0].displayName).toBe("User 1");
+    expect(usersFromDb[0].accountCreationDate).toEqual(new Date("2024-05-03T00:00:00.000+00:00"));
+    expect(usersFromDb[0].streakCount).toBe(2);
+    expect(usersFromDb[0].likedSongs).toEqual([new mongoose.Types.ObjectId("000000000000000000000001"), new mongoose.Types.ObjectId("000000000000000000000002")]);
+    expect(usersFromDb[0].suggestedSongs).toEqual([new mongoose.Types.ObjectId("000000000000000000000002"), new mongoose.Types.ObjectId("000000000000000000000003")]);
+    expect(usersFromDb[0].profilePic).toBe("profilePic1Source");
+
+    expect(usersFromDb[1].email).toBe("user2@test.com");
+    expect(usersFromDb[1].displayName).toBe("User 2");
+    expect(usersFromDb[1].accountCreationDate).toEqual(new Date("2024-05-02T00:00:00.000+00:00"));
+    expect(usersFromDb[1].streakCount).toBe(5);
+    expect(usersFromDb[1].likedSongs).toEqual([new mongoose.Types.ObjectId("000000000000000000000003"), new mongoose.Types.ObjectId("000000000000000000000004")]);
+    expect(usersFromDb[1].suggestedSongs).toEqual([new mongoose.Types.ObjectId("000000000000000000000003"), new mongoose.Types.ObjectId("000000000000000000000004")]);
+    expect(usersFromDb[1].profilePic).toBe("profilePic2Source");
+
+    expect(usersFromDb[2].email).toBe("user3@test.com");
+    expect(usersFromDb[2].displayName).toBe("User 3");
+    expect(usersFromDb[2].accountCreationDate).toEqual(new Date("2024-05-07T00:00:00.000+00:00"));
+    expect(usersFromDb[2].streakCount).toBe(0);
+    expect(usersFromDb[2].likedSongs).toEqual([]);
+    expect(usersFromDb[2].suggestedSongs).toEqual([]);
+    expect(usersFromDb[2].profilePic).toBe("profilePic3Source");
+});
