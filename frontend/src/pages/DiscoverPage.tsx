e1c6353e734e30978a07c539cdc742282abd6e6b
Lia Arroyo <liayzabel@gmail.com>
2024-05-09 00:59:43 +1200

    feature(discover-page): integrate discover page header with backend

--- frontend/src/pages/DiscoverPage.tsx
+++ frontend/src/pages/DiscoverPage.tsx
@@ -3,7 +3,7 @@ import SongSuggestionsContainer from "../components/SongSuggestionsContainer";
 import { styled } from "@mui/material";
 import useGet from "../utils/useGet";
 import LoadingSpinner from "../components/LoadingSpinner";
-import { SongSuggestionData } from "../utils/interfaces";
+import { SongData, SongSuggestionData } from "../utils/interfaces";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "";
 
@@ -14,16 +14,25 @@ const StyledDiscoverContainer = styled("div")({
 });
 
 const DiscoverPage = () => {
-  const { isLoading, data } = useGet<SongSuggestionData[]>({
+  const { isLoading: isFeedLoading, data: feedData } = useGet<
+    SongSuggestionData[]
+  >({
     url: `${API_BASE_URL}/feed`,
   });
+  const songs = feedData === null ? [] : feedData;
 
-  const songs = data === null ? [] : data;
+  // TODO: Replace with user ID from app context.
+  const userId = "663858af99e6720bfd8f96fb";
+  const { data: todaysSongSuggestionData } = useGet<SongData>({
+    url: `${API_BASE_URL}/user/${userId}/suggested/today`,
+  });
+  const todaysSongSuggestion =
+    todaysSongSuggestionData === null ? undefined : todaysSongSuggestionData;
 
   return (
     <StyledDiscoverContainer>
-      <DiscoverPageHeader />
-      {isLoading ? (
+      <DiscoverPageHeader songData={todaysSongSuggestion} />
+      {isFeedLoading ? (
         <LoadingSpinner />
       ) : (
         <SongSuggestionsContainer songSuggestionList={songs} />
