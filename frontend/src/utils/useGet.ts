0cc4471da043455a293914e7af9d27adf404ef9e
Lia Arroyo <liayzabel@gmail.com>
2024-05-08 17:30:48 +1200

    refactor(useGet): refactor to include query params and error

--- frontend/src/utils/useGet.ts
+++ frontend/src/utils/useGet.ts
@@ -1,23 +1,38 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { useState, useEffect, useRef } from "react";
 
 /**
  * @function useGet() hook
  * Fetches data from the given URL.
  *
  * @param T the type of data returned by the API
- * @param url, required string defining the path of the desired URL to GET from
- * @param initialState the data to get, initially null
+ * @param object containing:
+ *  the url - required string defining the path of the desired URL to GET from,
+ *  the initialState - (optional) the data to get - initially null,
+ *  the queryParams - (optional) the query parameters to send with the GET request
  *
  * @returns An object containing:
  *  the desired data,
  *  whether the data is still being loaded or not,
- *  a function to call to refresh the data, i.e. redo the GET request to the same URL (if URL hasn't changed)
+ *  a function to call to refresh the data, i.e. redo the GET request to the same URL (if URL hasn't changed),
+ *  an AxiosError object with error information
  */
-const useGet = <T>(url: string, initialState: T | null = null) => {
+const useGet = <T>({
+  url,
+  initialState = null,
+  queryParams = {},
+}: {
+  url: string;
+  initialState?: T | null;
+  queryParams?: Record<string, unknown>;
+}) => {
   const [data, setData] = useState(initialState);
   const [isLoading, setLoading] = useState<boolean>(false);
   const [refreshToggle, setRefreshToggle] = useState<boolean>(false);
+  const [error, setError] = useState<AxiosError>();
+
+  // To prevent unnecessary re-rendering due to queryParams
+  const queryParamsRef = useRef(queryParams);
 
   useEffect(() => {
     const source = axios.CancelToken.source();
@@ -25,10 +40,14 @@ const useGet = <T>(url: string, initialState: T | null = null) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(url, { cancelToken: source.token });
+        const response = await axios.get(url, {
+          cancelToken: source.token,
+          params: queryParamsRef.current,
+        });
         setData(response?.data);
       } catch (error) {
-        // catch cancel error when API request no longer needs to be made
+        setError(error as AxiosError);
+        // also catches cancel error when API request no longer needs to be made
       } finally {
         setLoading(false);
       }
@@ -38,12 +57,13 @@ const useGet = <T>(url: string, initialState: T | null = null) => {
     return () => {
       source.cancel("Request cancelled by cleanup");
     };
-  }, [url, refreshToggle]);
+  }, [url, refreshToggle, queryParamsRef]);
 
   const refresh = () => {
     setRefreshToggle(!refreshToggle);
   };
-  return { isLoading, data, refresh };
+
+  return { isLoading, data, refresh, error };
 };
 
 export default useGet;
