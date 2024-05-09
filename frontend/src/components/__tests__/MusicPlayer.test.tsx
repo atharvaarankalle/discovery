fae08b024059c06f7bcda63c64fab0faf85ec7d3
Amisha Singh <amishas171@gmail.com>
2024-05-09 21:02:41 +1200

    chore: set up vitest on frontend and test it works

---
+++ frontend/src/components/__tests__/MusicPlayer.test.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import { MusicPlayer } from "../MusicPlayer";
+
+it("tests that vitest is working", () => {
+  expect(1 + 1).toEqual(2);
+});
