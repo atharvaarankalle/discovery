a9daebfc1319ec7cc0a0e949f4f5d648f5a8c037
Michelle <100409601+mlie788@users.noreply.github.com>
2024-05-09 16:51:10 +1200

    feat: validation for user sign up form

--- frontend/src/components/SignupTab.tsx
+++ frontend/src/components/SignupTab.tsx
@@ -1,10 +1,11 @@
-import { Button, styled } from "@mui/material";
+import { Box, Button, styled, Typography } from "@mui/material";
 import CustomTextField from "./CustomTextField";
 
 import axios from "axios";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../AppContextProvider";
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "";
 
 const StyledButton = styled(Button)({
@@ -17,6 +18,9 @@ const SignupTab = () => {
   const [displayName, setDisplayName] = useState<string>("");
   const navigate = useNavigate();
   const { setCurrentUserId } = useContext(AppContext);
+  const [isEmailErrorState, setIsEmailErrorState] = useState(false);
+  const [isPasswordErrorState, setIsPasswordErrorState] = useState(false);
+  const [isErrorState, setIsErrorState] = useState(false);
 
   // This method does a POST request to signup and sets user ID in context if successful,
   // thus logging them in automatically after sign up.
@@ -25,12 +29,15 @@ const SignupTab = () => {
     password: string;
     displayName?: string;
   }) => {
+    console.log("click");
+
     try {
       const response = await axios.post(`${API_BASE_URL}/auth/signup`, signUp);
       setCurrentUserId(response.data.user._id); // sets the user ID in App Context (i.e. they are authenticated)
       navigate("/user/prompt");
     } catch (error: unknown) {
       console.error("Signup failed:", error);
+      setIsErrorState(true);
     }
   };
 
@@ -39,19 +46,48 @@ const SignupTab = () => {
       <CustomTextField
         label="Email"
         value={email}
+        required
         onChange={(event) => setEmail(event.target.value)}
+        error={isEmailErrorState || isErrorState}
+        onFocus={() => {
+          setIsEmailErrorState(false);
+          setIsErrorState(false);
+        }}
+        helperText={isEmailErrorState ? "Email is required" : null}
       />
       <CustomTextField
         label="Password"
         type="password"
         value={password}
+        required
         onChange={(event) => setPassword(event.target.value)}
+        error={isPasswordErrorState || isErrorState}
+        onFocus={() => {
+          setIsPasswordErrorState(false);
+          setIsErrorState(false);
+        }}
+        helperText={isPasswordErrorState ? "Password is required" : null}
       />
-      <CustomTextField
-        label="Display Name"
-        value={displayName}
-        onChange={(event) => setDisplayName(event.target.value)}
-      />
+      <Box sx={{ width: "100%" }}>
+        <CustomTextField
+          label="Display Name"
+          value={displayName}
+          error={isErrorState}
+          onChange={(event) => setDisplayName(event.target.value)}
+        />
+        {isErrorState && (
+          <Typography
+            sx={{
+              width: "100%",
+              mt: 1,
+              color: "error.main",
+              textAlign: "center",
+            }}
+          >
+            Looks like there's been an error signing up :( Please try again
+          </Typography>
+        )}
+      </Box>
       <StyledButton
         variant="contained"
         color="lightPeach"
@@ -61,6 +97,12 @@ const SignupTab = () => {
             setEmail("");
             setPassword("");
             setDisplayName("");
+          }
+          if (!email) {
+            setIsEmailErrorState(true);
+          }
+          if (!password) {
+            setIsPasswordErrorState(true);
           }
         }}
       >
